f42ce8b06dbb7d8eb879e9c0b8c7d7697f26aadd
PAT-FFM <118531774+PAT-FFM@users.noreply.github.com>
2023-01-06 19:19:40 +0100

    Update speaker.js

--- blocks/speaker/speaker.js
+++ blocks/speaker/speaker.js
@@ -1,1 +1,8 @@
-
+export default function decorate(block) {
+  /* change to ul, li */
+  const ul = document.createElement('ul');
+  const li = document.createElement('li');
+  li.innerHTML = "hello world";
+  ul.append(li);
+  block.append(ul);
+}
