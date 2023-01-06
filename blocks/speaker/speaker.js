86591f572d0e6ed513ff72759ad9d0f8b1a62d0c
PAT-FFM <118531774+PAT-FFM@users.noreply.github.com>
2023-01-06 21:41:31 +0100

    Update speaker.js

--- blocks/speaker/speaker.js
+++ blocks/speaker/speaker.js
@@ -1,14 +1,32 @@
 export default function decorate(block) {
-  const div = document.createElement('div');
-  div.innerHTML = `<a href="/speaker/speaker.json" target="_blank">Speaker Database</a>`;
-  block.append(div);
-  const ul = document.createElement('ul');
+  const div1 = document.createElement('div');
+  div1.innerHTML = `<a href="/speaker/speaker.json" target="_blank">Speaker Database</a>`;
+  block.append(div1);
+
+  const div2 = document.createElement('div');
+  div2.className="speaker-gallery";
+  
   fetch('/speaker/speaker.json')
   .then((response) => response.json())
   .then((data) => data.data.forEach( (speaker) => {
-    const li = document.createElement('li');
-    li.innerHTML = speaker.Page + `<img src="${speaker.Image}?width=400">`
-    ul.append(li);
+  
+    const div3 = document.createElement('div');
+    div3.innerHTML = `<div class="speaker-gallery-item">
+        <div class="box-padding">
+          <div class="speaker-image">
+            <a href="https://adapt.to/2021/en/conference/speaker.detail.suffix.html/alexander-saar.html">
+            <img src="/content/dam/adaptto/production/speaker-portraits/2020-Alexander-Saar.jpg/_jcr_content/renditions/original./2020-Alexander-Saar.jpg" alt="Alexander Saar" 
+            </a>
+          </div>
+          <div class="speaker-data">
+            <div class="speaker-name">
+            <a href="https://adapt.to/2021/en/conference/speaker.detail.suffix.html/alexander-saar.html">Alexander Saar</a>
+            </div>
+            <div class="speaker-company">Adobe</div>
+          </div>
+        </div>
+        </div>`
+    div2.append(div3);
   }));
-  block.append(ul);
+  block.append(div2);
 }
