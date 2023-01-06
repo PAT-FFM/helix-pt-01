4f0dcac46f151e020a1d970bd205407112454e95
PAT-FFM <118531774+PAT-FFM@users.noreply.github.com>
2023-01-06 23:04:34 +0100

    Update speaker.js

--- blocks/speaker/speaker.js
+++ blocks/speaker/speaker.js
@@ -1,18 +1,15 @@
 export default function decorate(block) {
+ 
   const div1 = document.createElement('div');
-  div1.innerHTML = `<a href="/speaker/speaker.json" target="_blank">Speaker Database</a>`;
-  block.append(div1);
-
-  const div2 = document.createElement('div');
-  div2.className="speaker-gallery";
+  div1.className="speaker-gallery";
   
   fetch('/speaker/speaker.json')
   .then((response) => response.json())
   .then((data) => data.data.forEach( (speaker) => {
   
-    const div3 = document.createElement('div');
-    div3.className="speaker-gallery-item";
-    div3.innerHTML = `<div class="speaker-image">
+    const div2 = document.createElement('div');
+    div2.className="speaker-gallery-item";
+    div2.innerHTML = `<div class="speaker-image">
             <a href="/speaker/${speaker.Page}">
             <img src="${speaker.Image}?width=400" alt="${speaker.Firstname} ${speaker.Lastname}"/>
             </a>
@@ -23,7 +20,7 @@ export default function decorate(block) {
             </div>
             <div class="speaker-company">${speaker.Company}</div>
           </div>`
-    div2.append(div3);
+    div1.append(div2);
   }));
-  block.append(div2);
+  block.append(div1);
 }
