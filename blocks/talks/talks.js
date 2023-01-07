5defd6ce9d476c60daf6e399da364b482242d78c
PAT-FFM <118531774+PAT-FFM@users.noreply.github.com>
2023-01-07 23:38:25 +0100

    Update talks.js

--- blocks/talks/talks.js
+++ blocks/talks/talks.js
@@ -2,7 +2,7 @@ export default function decorate(block) {
 
   const speakername = document.getElementsByTagName("h1")[0].innerText;
   const h4 = document.createElement('h4');
-  h4.innerHTML="Talks by " + speakername;
+  h4.innerHTML="Talks by " + speakername; 
   block.append(h4);
   const ul = document.createElement('ul');
   
@@ -11,8 +11,9 @@ export default function decorate(block) {
   .then( x => x.data.filter(t => t.speaker === speakername).forEach( (talk) => {
     const li = document.createElement('li');
     li.className="talk-item";
-    li.innerHTML = `<a href="${talk.talk.split('|')[1]}">${talk.talk.split('|')[0]}</a> ${talk.year}`
+    li.innerHTML = `<a href="${talk.talk.split('|')[1]}">${talk.talk.split('|')[0]}</a> (${talk.year})`
     ul.append(li);
  }));
+  
   block.append(ul);
 }
