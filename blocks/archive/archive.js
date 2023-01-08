dfb2c0a4fe16403ae4d8e5a98fbd7d6d99345bc4
PAT-FFM <118531774+PAT-FFM@users.noreply.github.com>
2023-01-08 13:33:22 +0100

    Update archive.js

--- blocks/archive/archive.js
+++ blocks/archive/archive.js
@@ -1,18 +1,15 @@
 export default function decorate(block) {
   
   block.innerHTML= `
-  <div class="contentHeadline section"><h3 class="title title-section">Talk Archive</h3></div>
-<div class="contentRichText section">
-<p>This is our talk archive with a list of all adaptTo() talks since 2011.<br /></p>
-</div>
-<div class="talkarchive section">
+  <h3 class="title title-section">Talk Archive</h3>
+  <p>This is our talk archive with a list of all adaptTo() talks since 2011.<br /></p>
   <div class="interactive_table row box-padding" data-json-url="https://main--helix-pt-01--pat-ffm.hlx.page/talks/talks-01.json">
     <div class="col-5-5 textsearch">
 	&#x1F50D; <input type="search" placeholder='e.g. &quot;code base&quot;' class="searchfield"/>
-	</div>
-	<div class="col-1-5 filter_wrapper"></div>
-	<div class="col-4-5 table_wrapper"><div>
-	<table>
+    </div>
+    <div class="col-1-5 filter_wrapper"></div>
+    <div class="col-4-5 table_wrapper"><div>
+        <table>
 	<thead>
 	<tr>
 	<th>Year</th>
@@ -21,7 +18,5 @@ export default function decorate(block) {
 	</tr>
 	</thead>
 	</table>
-  </div>
-</div>
-`;
+  </div>`;
 }
