b53a43ca0fb4a96dd2de6c080636b57a938bf9eb
PAT-FFM <118531774+PAT-FFM@users.noreply.github.com>
2023-01-08 18:03:58 +0100

    Update custom.js

--- scripts/custom.js
+++ scripts/custom.js
@@ -861,7 +861,7 @@ h.each(n,function(a,b){h.fn.DataTable[a]=b});return h.fn.dataTable});
                 </div>
                 </div>
               `;
-             $loadTarget = overlayContent;
+             $loadTarget.html(overlayContent);
              plugin.loadComplete();
              $(document).trigger("ajax.loaded", $loadTarget);
           }
