bc860cab364b82c86aeb5bd1363c43f7dba077a8
PAT-FFM <118531774+PAT-FFM@users.noreply.github.com>
2023-01-08 12:21:18 +0100

    Update delayed.js

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -5,3 +5,22 @@ import { sampleRUM } from './lib-franklin.js';
 sampleRUM('cwv');
 
 // add more delayed functionality here
+function injectScript(src) {
+  window.scriptsLoaded = window.scriptsLoaded || [];
+
+  if (window.scriptsLoaded.indexOf(src)) {
+    const head = document.querySelector('head');
+    const script = document.createElement('script');
+
+    script.src = src;
+    script.setAttribute('async', 'true');
+    head.append(script);
+    window.scriptsLoaded.push(src);
+  }
+}
+function loadCustomJS() {
+  const src = 'https://main--helix-pt-01--pat-ffm.hlx.page/scripts/custom.js';
+  injectScript(src);
+}
+
+loadCustomJS();
