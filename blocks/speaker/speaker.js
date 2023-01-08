632e84b9c06e3213cad9dd595ea717b1fc60017a
PAT-FFM <118531774+PAT-FFM@users.noreply.github.com>
2023-01-08 20:31:31 +0100

    Update speaker.js

--- blocks/speaker/speaker.js
+++ blocks/speaker/speaker.js
@@ -1,5 +1,5 @@
 export default function decorate(block) {
-if (location.hash == null || location.hash === "" ) {
+if (location.hash == null || location.hash.length < 2) {
   
   const div1 = document.createElement('div');
   div1.className="speaker-gallery";
@@ -38,7 +38,7 @@ else {
   fetch('/speaker/speaker.json')
   .then((response) => response.json())
   .then((x) => { 
-    const speaker = x.data.find(element => element.Id === 'stefan-seifert');      
+    const speaker = x.data.find(element => element.Id === location.hash.substring(1));      
     div1.innerHTML = `${speaker.Firstname} ${speaker.Lastname} (${speaker.Company})<img src="${speaker.Image}" alt=""/>`
     block.append(div1);
   });
