8b993eec46c2f491d574cc8886a94e46057aac61
PAT-FFM <118531774+PAT-FFM@users.noreply.github.com>
2023-01-08 18:17:55 +0100

    Update gallery.js

--- blocks/gallery/gallery.js
+++ blocks/gallery/gallery.js
@@ -1,39 +1,46 @@
 export default function decorate(block) {
-  
+ 
+  const ul = document.createElement('ul');
+  ul.className="gallery_thumb_list";
+
+  fetch('/gallery/gallery.json')
+  .then((response) => response.json())
+  .then( x => x.data.forEach( (item) => {
+    const li = document.createElement('li');
+    li.className="gallery-item";
+    li.innerHTML = `
+        <a class="gallery_thumb" href="https://main--helix-pt-01--pat-ffm.hlx.page/${item.path}" 
+            rel="gallery" data-overlay="https://main--helix-pt-01--pat-ffm.hlx.page/${item.path}">
+        <img src="https://main--helix-pt-01--pat-ffm.hlx.page/${item.path}?width=200"/>
+        </a>
+    `;
+    ul.append(li);
+ }));
+ 
   block.innerHTML= `
-<section id="stage" class="col-3-4">
-  <article class="box-padding">
-  <div class="contentHeadline section"><h1 class="title title-section">Gallery</h1>
-</div>
-  <div class="contentRichText section">
-  <p>Photos from adaptTo() 2021 Online Conference - &quot;behind the scenes&quot;.<br />
-</p>
-</div>
-  <div class="stagegallery section"><div class="stage">
-  <div class="image_gallery">
-    <div class="gallery_stage">
-      <a class="gallery__prev">Previous</a>
-      <div class="gallery_placeholder">
-        <img class="gallery_image" src=""/>
-      </div>
-      <a class="gallery__next">Next</a>
-      <a class="gallery_fullscreen_btn" href="" title="Fullscreen">Fullscreen</a>
+    <section id="stage" class="col-3-4">
+      <article class="box-padding">
+      <div class="contentHeadline section"><h1 class="title title-section">Gallery</h1>
     </div>
-    <ul class="gallery_thumb_list">
-      <li><a class="gallery_thumb" href="https://main--helix-pt-01--pat-ffm.hlx.page/media_1dc9f915d9ae600ec2dbbf8e1a0b1bb6b4302b2f2.jpeg" 
-        rel="gallery" data-overlay="https://main--helix-pt-01--pat-ffm.hlx.page/media_1dc9f915d9ae600ec2dbbf8e1a0b1bb6b4302b2f2.jpeg">
-        <img src="https://main--helix-pt-01--pat-ffm.hlx.page/media_1dc9f915d9ae600ec2dbbf8e1a0b1bb6b4302b2f2.jpeg?width=500"/></a></li>
-      <li><a class="gallery_thumb" href="https://main--helix-pt-01--pat-ffm.hlx.page/media_1dc9f915d9ae600ec2dbbf8e1a0b1bb6b4302b2f2.jpeg" 
-        rel="gallery" data-overlay="https://main--helix-pt-01--pat-ffm.hlx.page/media_1dc9f915d9ae600ec2dbbf8e1a0b1bb6b4302b2f2.jpeg">
-        <img src="https://main--helix-pt-01--pat-ffm.hlx.page/media_1dc9f915d9ae600ec2dbbf8e1a0b1bb6b4302b2f2.jpeg?width=500"/></a></li> 
-      <li><a class="gallery_thumb" href="https://main--helix-pt-01--pat-ffm.hlx.page/media_1dc9f915d9ae600ec2dbbf8e1a0b1bb6b4302b2f2.jpeg" 
-        rel="gallery" data-overlay="https://main--helix-pt-01--pat-ffm.hlx.page/media_1dc9f915d9ae600ec2dbbf8e1a0b1bb6b4302b2f2.jpeg">
-        <img src="https://main--helix-pt-01--pat-ffm.hlx.page/media_1dc9f915d9ae600ec2dbbf8e1a0b1bb6b4302b2f2.jpeg?width=500"/></a></li>        
-    </ul>
-  </div>
-</div>
-</div>
-</article>
-</section>
-`;
+      <div class="contentRichText section">
+      <p>Photos from adaptTo() 2021 Online Conference - &quot;behind the scenes&quot;.<br />
+    </p>
+    </div>
+      <div class="stagegallery section"><div class="stage">
+      <div class="image_gallery">
+        <div class="gallery_stage">
+          <a class="gallery__prev">Previous</a>
+          <div class="gallery_placeholder">
+            <img class="gallery_image" src=""/>
+          </div>
+          <a class="gallery__next">Next</a>
+          <a class="gallery_fullscreen_btn" href="" title="Fullscreen">Fullscreen</a>
+        </div>
+        ${ul.outerHTML}
+      </div>
+    </div>
+    </div>
+    </article>
+    </section>
+    `;
 }
